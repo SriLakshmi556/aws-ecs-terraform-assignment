6fb43f6ffb94102e0d13b87dec29587ec35a506b
Nagasri Lakshmi <vnagasrilakshmi@deloitte.com>
2023-03-02 12:03:03 +0530

    Working code

---
+++ data.tf
@@ -0,0 +1,36 @@
+locals {
+        vpc_name = "vpc"
+}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [local.vpc_name]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["${local.vpc_name}-subnet-public*"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["${local.vpc_name}-subnet-private*"]
+  }
+}
+
+data "aws_caller_identity" "current" {}
